6c8fd06eca3ba7e27c8290fbeee1f29ec5c7cf8e
SunDev <sundly@live.fr>
2025-09-07 19:36:00 +0200

    Ajout doss models + models.ts

---
+++ koosy/models/models.ts
@@ -0,0 +1,61 @@
+export interface Commentaire {
+  id: string;
+  texte: string;
+  date: string;
+}
+export interface Proprio {
+  id: string;
+  nom: string;
+  email: string;
+  telephone: string;
+}
+// Définition des types pour la gestion des biens, locataires et tâches
+
+export interface Bien {
+  id: string;
+  nom: string;
+  adresse: string;
+  type: string;
+  superficie: number;
+  pieces: number;
+  equipements: string[];
+  photos: string[];
+  locataires: Locataire[];
+  taches: Tache[];
+  geo: { lat: number; lng: number };
+  statut: 'disponible' | 'occupé' | 'travaux';
+  historique: any[];
+  proprio: Proprio;
+  commentaires?: Commentaire[];
+}
+
+export interface Locataire {
+  id: string;
+  nom: string;
+  email: string;
+  telephone: string;
+  dateArrivee?: string;
+  dateDepart?: string;
+  bienId?: string;
+}
+
+export interface Tache {
+  id: string;
+  titre: string;
+  description: string;
+  statut: 'à faire' | 'en cours' | 'terminée';
+  bienId: string;
+  dateEcheance?: string;
+}
+
+
+export interface Reservation {
+  id: string;
+  bienId: string; //le logement réservé
+  locataireId: string; //la personne qui réserve
+  dateArrivee: string; //quand la personne commence
+    dateDepart: string; //quand la personne finit
+    heureArrivee: string; // Heure d'arrivée (format HH:mm)
+  heureDepart: string;  // Heure de départ (format HH:mm)
+  statut: 'confirmée' | 'en attente' | 'annulée';
+}
