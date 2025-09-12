a8258cfba756aec6a9a3b6de032f673882a0cdab
SundLyDev <167065053+Sullivankow@users.noreply.github.com>
2025-09-12 11:56:53 +0200

    Désactivation onsave + reconfig eslint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -4,13 +4,12 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
   {
     ignores: ['eslint.config.mjs'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
@@ -28,7 +27,8 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
-);
+  eslintPluginPrettierRecommended,
+];
