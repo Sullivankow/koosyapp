b291fa6c56bb281eb3c7a2b89222b177dfc872ff
SundLyDev <167065053+Sullivankow@users.noreply.github.com>
2025-09-19 18:27:11 +0200

    Ajout get et delete image + test api OK

--- backend/src/Image/bien-image/bien-image.controller.ts
+++ backend/src/Image/bien-image/bien-image.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UseGuards, UseInterceptors, UploadedFile, Param, Inject } from '@nestjs/common';
+import { Controller, Post, Get, Delete, UseGuards, UseInterceptors, UploadedFile, Param, Inject } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 import { ApiBearerAuth, ApiConsumes, ApiBody, ApiTags, ApiResponse } from '@nestjs/swagger';
@@ -13,9 +13,6 @@ export class BienImageController {
 
   /**
    * Upload d'une image pour un bien
-   * @param bienId id du bien
-   * @param file fichier image (multipart/form-data)
-   * @returns infos sur le fichier uploadé et l'entité créée
    */
   @Post('biens/:bienId/images')
   @UseGuards(JwtAuthGuard)
@@ -38,6 +35,29 @@ export class BienImageController {
   async uploadImage(@Param('bienId') bienId: string, @UploadedFile() file: any) {
     const image = await this.bienImageService.addImageToBien(Number(bienId), file.path);
     return { bienId, filename: file.filename, path: file.path, image };
+  }
+
+  /**
+   * Récupérer toutes les images d'un bien
+   */
+  @ApiResponse({ status: 200, description: 'Liste des images du bien.' })
+  @ApiResponse({ status: 401, description: 'Non authentifié.' })
+  @UseGuards(JwtAuthGuard)
+  @Get('biens/:bienId/images')
+  async getImages(@Param('bienId') bienId: string) {
+    const images = await this.bienImageService.getImagesOfBien(Number(bienId));
+    return images;
+  }
+
+  /**
+   * Supprimer une image
+   */
+  @ApiResponse({ status: 200, description: 'Image supprimée avec succès.' })
+  @ApiResponse({ status: 401, description: 'Non authentifié.' })
+  @UseGuards(JwtAuthGuard)
+  @Delete('images/:imageId')
+  async deleteImage(@Param('imageId') imageId: string) {
+    return await this.bienImageService.deleteImage(Number(imageId));
   }
 
 }
