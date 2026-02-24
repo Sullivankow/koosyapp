540883f291027af26fcb6ead648441218112e8d5
SundLyDev <167065053+Sullivankow@users.noreply.github.com>
2026-02-24 21:32:08 +0100

    update restructuration OK

--- koosy/screens/navigation/ParametresStack.tsx
+++ koosy/screens/navigation/ParametresStack.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import ParametresScreen from '../Layout/ParametresScreen';
 import ProfilScreen from '../Settings/ProfilScreen';
-import PreferencesScreen from '../PreferencesScreen';
 import NotificationsScreen from '../Settings/NotificationsSettingsScreen';
 import SecuriteScreen from '../Settings/SecuriteScreen';
 import AProposScreen from '../Settings/AProposScreen';
@@ -15,7 +14,6 @@ const ParametresStack: React.FC = () => (
     <Stack.Navigator>
         <Stack.Screen name="Parametres" component={ParametresScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Profil" component={ProfilScreen} />
-        <Stack.Screen name="Preferences" component={PreferencesScreen} />
         <Stack.Screen name="Notifications" component={NotificationsScreen} />
         <Stack.Screen name="Securite" component={SecuriteScreen} />
         <Stack.Screen name="APropos" component={AProposScreen} />
